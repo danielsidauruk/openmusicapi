fc7c6f0279ebe1f58b725342cc350461bd80a14b
Daniel Sidauruk <macbook@Daniel.local>
2024-08-01 18:24:14 +0700

    chore: add album handler

---
+++ src/api/albums/handler.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-unused-vars */
+class AlbumsHandler {
+  constructor(service, validator) {
+    this._service = service;
+    this._validator = validator;
+
+    this.postAlbumHandler = this.postAlbumHandler.bind(this);
+    this.getAlbumsHandler = this.getAlbumsHandler.bind(this);
+    this.putAlbumByIdHandler = this.putAlbumByIdHandler.bind(this);
+    this.deleteAlbumByIdHandler = this.deleteAlbumByIdHandler.bind(this);
+  }
+
+  async postAlbumHandler(request, h) {
+    this._validator.validateAlbumPayload(request.payload);
+    const { name, year } = request.payload;
+
+    const albumId = await this._service.addAlbum({ name, year });
+
+    const response = h.response({
+      status: 'success',
+      message: 'Album successfully added.',
+      data: {
+        albumId,
+      },
+    });
+
+    response.code(201);
+    return response;
+  }
+
+  async getAlbumsHandler() {
+    const albums = await this._service.getAlbums();
+    return {
+      status: 'success',
+      data: {
+        albums,
+      },
+    };
+  }
+
+  async putAlbumByIdHandler(request, h) {
+    this._validator.validateAlbumPayload(request.payload);
+    const { id } = request.params;
+
+    await this._service.editAlbumById(id, request.payload);
+
+    return {
+      status: 'success',
+      message: 'Album updated successfully.',
+    };
+  }
+
+  async deleteAlbumByIdHandler(request, h) {
+    const { id } = request.params;
+    await this._service.deleteAlbumById(id);
+    return {
+      status: 'success',
+      message: 'Album deleted successfilly.',
+    };
+  }
+}
+
+module.exports = AlbumsHandler;
