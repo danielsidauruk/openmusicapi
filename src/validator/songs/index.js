9f17ef676ef0afd2de8c2e1be3a580bdad81d07b
Daniel Sidauruk <macbook@Daniel.local>
2024-08-02 22:56:49 +0700

    fix: fix property in SongValidator

--- src/validator/songs/index.js
+++ src/validator/songs/index.js
@@ -2,7 +2,7 @@ const { SongPayloadSchema } = require('./schema');
 const InvariantError = require('../../exceptions/InvariantError');
 
 const SongsValidator = {
-  validateAlbumPayload: (payload) => {
+  validateSongPayload: (payload) => {
     const validationResult = SongPayloadSchema.validate(payload);
     if (validationResult.error) {
       throw new InvariantError(validationResult.error.message);
